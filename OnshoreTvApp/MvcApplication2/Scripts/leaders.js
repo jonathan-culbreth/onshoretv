cb9aeb69620a3cd646bb3877d810e18e4c5f42e3
Dani <danielle.reinking@onshoreoutsourcing.com>
2014-08-25 07:42:31 -0400

    Fixed weird "hotdog effect" on What We Do

--- OnshoreTvApp/MvcApplication2/Scripts/leaders.js
+++ OnshoreTvApp/MvcApplication2/Scripts/leaders.js
@@ -1,7 +1,7 @@
-$('#shane')
-    .hide()
-    .show('fold', 2500);
-$('#chuck')
-    .hide()
-    .delay(2500)
-    .show('fold', 2500);
+//$('#shane')
+//    .hide()
+//    .show('fold', 2500);
+//$('#chuck')
+//    .hide()
+//    .delay(2500)
+//    .show('fold', 2500);
